aa72c88d529a58118bf656ece164167e679dd506
Mark Petersen <mark@mtpetersen.com>
2015-10-20 15:22:54 +0200

    initial changes d2att towards d2dcm
    Change-Id: Ibc2b5968f1ff32952d63b24b4d23b37425a2f495

--- device.mk
+++ device.mk
@@ -18,7 +18,7 @@
 $(call inherit-product-if-exists, vendor/samsung/d2gsm/d2gsm-vendor.mk)
 
 ## overlays
-DEVICE_PACKAGE_OVERLAYS += device/samsung/d2att/overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/d2dcm/overlay
 
 # Inherit from d2-common
 $(call inherit-product, device/samsung/d2-common/d2-common.mk)
--- full_d2dcm.mk
+++ full_d2dcm.mk
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
- 
+
 #
 # This file is the build configuration for a full Android
 # build for maguro hardware. This cleanly combines a set of
@@ -20,15 +20,15 @@
 # details, it only fundamentally contains two inherit-product
 # lines, full and maguro, hence its name.
 #
- 
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d2att device
-$(call inherit-product, device/samsung/d2att/device.mk)
+$(call inherit-product, device/samsung/d2dcm/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_d2att
-PRODUCT_DEVICE := d2att
+PRODUCT_NAME := full_d2dcm
+PRODUCT_DEVICE := d2dcm
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := SAMSUNG-SGH-I747
+PRODUCT_MODEL := SC-06D
