c17a81412dcba7f349fd232d40cb39ba29699ae4
Mark Petersen <mark@mtpetersen.com>
2015-10-21 10:16:59 +0200

    fix comment
    Change-Id: I2c39d51ea3780cbde158e5cac91f94f1064f7258

--- full_d2dcm.mk
+++ full_d2dcm.mk
@@ -23,7 +23,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-# Inherit from d2att device
+# Inherit from d2dcm device
 $(call inherit-product, device/samsung/d2dcm/device.mk)
 
 # Set those variables here to overwrite the inherited values.
